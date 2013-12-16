d2d3878ed58fc8a6a99ed7bee458ef7010024240
msunda <meera.sundar@gmail.com>
2013-12-16 07:51:19 +0530

    Initial Commit

---
+++ MultiScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultiScroll
+//
+//  Created by Meera Sundar on 16/12/13.
+//  Copyright (c) 2013 Meera Sundar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultiScroll/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultiScroll
+//
+//  Created by Meera Sundar on 16/12/13.
+//  Copyright (c) 2013 Meera Sundar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
